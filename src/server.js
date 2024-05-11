ad3d1a440e02677174bd2be6de20e9a075f84e46
Jeminah Nicole Pausal <mahumotkevin223@gmail.com>
2024-05-11 18:18:23 +0800

    API URL

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const serverURL = `${window.location.protocol}//${window.location.host.replace(/:\d+$/, '')}`;
-const API_URL = `${serverURL}:5000/`;
+// const serverURL = `${window.location.protocol}//${window.location.host.replace(/:\d+$/, '')}`;
+// const API_URL = `${serverURL}:5000/`;
+const API_URL = 'https://backend-41i7.onrender.com/';
 
 export { API_URL};
